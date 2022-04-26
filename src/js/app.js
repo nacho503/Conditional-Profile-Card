f37bc2e6ba8d5fe0f58e513cae0aba1d7a970e74
nacho503 <ignacio.rosenberg.503@gmail.com>
2022-04-26 00:41:31 +0000

    tercer commit con links arreglados

--- src/js/app.js
+++ src/js/app.js
@@ -41,18 +41,18 @@ function render(variables = {}) {
     variables.country == null ? "" : variables.country
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy">${
+            <li><a href="https://twitter.com/${
               variables.twitter == null ? "" : variables.twitter
-            }<i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy">${
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
               variables.github == null ? "" : variables.github
-            }<i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy">${
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
               variables.linkedin == null ? "" : variables.linkedin
-            }<i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy">${
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
               variables.instagram == null ? "" : variables.instagram
-            }<i class="fab fa-instagram"></i></a></li>
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
